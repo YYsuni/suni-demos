a81222746bc0ecc0bd968c76807f669d840dc143
Suni <YYsuni1001@gmail.com>
2025-10-09 16:37:39 +0800

    First commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+	reactStrictMode: false,
+	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+	experimental: {
+		scrollRestoration: false
+	},
+	turbopack: {
+		rules: {
+			'*.svg': {
+				loaders: ['@svgr/webpack'],
+				as: '*.js'
+			}
+		},
+
+		resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json', 'css']
+	},
+	webpack: config => {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			use: [{ loader: '@svgr/webpack', options: { svgo: false } }]
+		})
+
+		return config
+	}
 };
 
 export default nextConfig;
